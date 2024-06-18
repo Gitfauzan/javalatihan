a58a483c1de196857dd26678491746a58ddeaddc
Gitfauzan <muhammad.alif.fauzan@students.amikom.ac.id>
2024-06-18 00:56:19 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+
+import LearningtheJavaLanguage.AObjectOrientedProgrammingConcepts.elang;
+import essentialJavaClasses.exceptions.ListOfNumbers;
+
+public class Main {
+    public static void main(String[] args) {
+        elang elangjawa = new elang("elang Jawa");
+        elangjawa.makandaging();
+        elangjawa.terbang();
+    }
+}
